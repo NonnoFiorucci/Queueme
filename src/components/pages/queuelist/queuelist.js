a2eeda14c5aa36158a7373b4db9af263e43324c3
Vincenzo Chiarella <vins14nov@outlook.it>
2019-05-13 18:39:33 +0200

    fix Queue List

--- src/components/pages/queuelist/queuelist.js
+++ src/components/pages/queuelist/queuelist.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { fire } from '../../../config/FirebaseConfig';
 
 
-import { TiDeleteOutline } from 'react-icons/ti';
+//import { TiDeleteOutline } from 'react-icons/ti';
 import { TiPlus } from 'react-icons/ti';
-import { FiSend } from 'react-icons/fi';
+//import { FiSend } from 'react-icons/fi';
 
 
 
 
-import { Card, Button, Form } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 
 
@@ -21,15 +21,18 @@ class ListaCode extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      expanded: false,
-      codice: [],
-      number: [],
-
-      titletxt: [],
+      expanded: false,     
+      
+      title: [],
+      idCompany: [],
+      idQueue: [],
+      data: [],
       description: [],
       image: [],
-      idCompany: [],
-      data: [],
+      numWait: [],
+      active: [],
+
+
       key: 'home',
       indexModal: null,
     };
@@ -38,115 +41,115 @@ class ListaCode extends Component {
 
   }
 
-  handleExpandClick = () => {
-    this.setState(state => ({ expanded: !state.expanded }));
-  };
+  // handleExpandClick = () => {
+  //   this.setState(state => ({ expanded: !state.expanded }));
+  // };
 
   showQueue() {
     const QueueRef = fire.database().ref();
-    const queue = QueueRef.child('queue/')
+    const queue = QueueRef.child('queues/')
 
     queue.once('value', snap => {
       snap.forEach(child => {
         this.setState({
-          codice: this.state.codice.concat([child.key]),
-          number: this.state.number.concat([child.val().number]),
-
-          titletxt: this.state.titletxt.concat([child.val().titletxt]),
+          idQueue: this.state.idQueue.concat([child.key]),
+          numWait: this.state.numWait.concat([child.val().numWait]),
+          title: this.state.title.concat([child.val().title]),
           description: this.state.description.concat([child.val().description]),
           image: this.state.image.concat([child.val().image]),
+          active: this.state.active.concat([child.val().active]),
         });
       });
     });
   }
 
 
-  writeQueue(codice, idAzienda, Titolo, Descrizione, Immagine) {
-    fire.database().ref('queue/' + codice).set({
-      idCompany: idAzienda,
-      titletxt: Titolo,
-      description: Descrizione,
-      number: 0,
-      image: Immagine,
-    }).then((data) => {
-      //success callback
-      console.log('data ', data)
-    }).catch((error) => {
-      //error callback
-      console.log('error ', error)
-    })
-  }
+  // writeQueue(codice, idAzienda, Titolo, Descrizione, Immagine) {
+  //   fire.database().ref('queue/' + codice).set({
+  //     idCompany: idAzienda,
+  //     titletxt: Titolo,
+  //     description: Descrizione,
+  //     number: 0,
+  //     image: Immagine,
+  //   }).then((data) => {
+  //     //success callback
+  //     console.log('data ', data)
+  //   }).catch((error) => {
+  //     //error callback
+  //     console.log('error ', error)
+  //   })
+  // }
 
   uniqueIDCode() {
     var ID = Date.now();
     return ID;
   }
 
-  addQueue() {
-    /* const codice = this.accountInput.value
-    const idUtente = this.usernameInput.value */
-    const titolo = this.Titolo.value
-    const descrizione = this.Descrizione.value
+  // addQueue() {
+  //   /* const codice = this.accountInput.value
+  //   const idUtente = this.usernameInput.value */
+  //   const titolo = this.Titolo.value
+  //   const descrizione = this.Descrizione.value
 
-    const image = this.Immagine.value
+  //   const image = this.Immagine.value
 
-    const numero = 0;
+  //   const numero = 0;
 
-    const codiceQueue = this.uniqueIDCode();
-    //const data = this.getData();
-    if (titolo !== '') {
-      if (codiceQueue !== '' && codiceQueue !== null) {
-        this.writeQueue(codiceQueue, this.props.userID, titolo, descrizione, numero, image) //id=this.state.userID
-        alert('Coda ' + codiceQueue + ' inviata correttamente')
-      } else {
-        alert('Errore generazione codice coda, riprova')
-      }
-    } else {
-      alert("Tutti i campi devono essere compilati")
-    }
-    this.segnForm.reset();
-  }
+  //   const codiceQueue = this.uniqueIDCode();
+  //   //const data = this.getData();
+  //   if (titolo !== '') {
+  //     if (codiceQueue !== '' && codiceQueue !== null) {
+  //       this.writeQueue(codiceQueue, this.props.userID, titolo, descrizione, numero, image) //id=this.state.userID
+  //       alert('Coda ' + codiceQueue + ' inviata correttamente')
+  //     } else {
+  //       alert('Errore generazione codice coda, riprova')
+  //     }
+  //   } else {
+  //     alert("Tutti i campi devono essere compilati")
+  //   }
+  //   this.segnForm.reset();
+  // }
 
-  resetForm() {
-    this.segnForm.reset();
-  }
-
-
+  // resetForm() {
+  //   this.segnForm.reset();
+  // }
 
 
-  getQueueForm() {
-    return (
-      <div className="QueueForm">
-        <Form onSubmit={() => { this.addQueue() }} ref={(form) => { this.segnForm = form }}>
-          <Card  >
 
-            <Card.Header style={{ fontWeight: 'bold' }}>Crea coda</Card.Header>
-            <Card.Body>
-              <Card.Text>
-                <Form.Group controlId="formBasicInput">
 
-                  <Form.Group className="testoForm" controlId="formBasicInput">
-                    <Form.Label style={{ fontWeight: 'bold' }}> Titolo: </Form.Label>
-                    <Form.Control className="testoForm" as="textarea" rows="1" ref={(input) => { this.Titolo = input }} />
-                    <Form.Label style={{ fontWeight: 'bold' }}> Descrizione: </Form.Label>
-                    <Form.Control className="testoForm" as="textarea" rows="2" ref={(input2) => { this.Descrizione = input2 }} />
-                    <Form.Label style={{ fontWeight: 'bold' }}> Immagine: </Form.Label>
-                    <Form.Control className="testoForm" as="textarea" rows="2" ref={(input3) => { this.Immagine = input3 }} />
-                  </Form.Group>
-                </Form.Group>
-              </Card.Text>
-              <Button variant="success" style={{ fontWeight: 'bold' }} className="segnalazioneButton" type="submit">Invia<FiSend className="blogIcon" />
-              </Button>
-              <Button variant="danger" style={{ fontWeight: 'bold' }} className="segnalazioneButton"
-                onClick={() => { this.resetForm() }} ref={(form) => { this.segnForm = form }}>Reset Dati Inseriti 
-                    <TiDeleteOutline className="blogIcon" />
-              </Button>
-            </Card.Body>
-          </Card>
-        </Form>
-      </div>
-    )
-  }
+  // getQueueForm() {
+  //   return (
+  //     <div className="QueueForm">
+  //       <Form onSubmit={() => { this.addQueue() }} ref={(form) => { this.segnForm = form }}>
+  //         <Card  >
+
+  //           <Card.Header style={{ fontWeight: 'bold' }}>Crea coda</Card.Header>
+  //           <Card.Body>
+  //             <Card.Text>
+  //               <Form.Group controlId="formBasicInput">
+
+  //                 <Form.Group className="testoForm" controlId="formBasicInput">
+  //                   <Form.Label style={{ fontWeight: 'bold' }}> Titolo: </Form.Label>
+  //                   <Form.Control className="testoForm" as="textarea" rows="1" ref={(input) => { this.Titolo = input }} />
+  //                   <Form.Label style={{ fontWeight: 'bold' }}> Descrizione: </Form.Label>
+  //                   <Form.Control className="testoForm" as="textarea" rows="2" ref={(input2) => { this.Descrizione = input2 }} />
+  //                   <Form.Label style={{ fontWeight: 'bold' }}> Immagine: </Form.Label>
+  //                   <Form.Control className="testoForm" as="textarea" rows="2" ref={(input3) => { this.Immagine = input3 }} />
+  //                 </Form.Group>
+  //               </Form.Group>
+  //             </Card.Text>
+  //             <Button variant="success" style={{ fontWeight: 'bold' }} className="segnalazioneButton" type="submit">Invia<FiSend className="blogIcon" />
+  //             </Button>
+  //             <Button variant="danger" style={{ fontWeight: 'bold' }} className="segnalazioneButton"
+  //               onClick={() => { this.resetForm() }} ref={(form) => { this.segnForm = form }}>Reset Dati Inseriti 
+  //                   <TiDeleteOutline className="blogIcon" />
+  //             </Button>
+  //           </Card.Body>
+  //         </Card>
+  //       </Form>
+  //     </div>
+  //   )
+  // }
 
 
 
@@ -154,23 +157,21 @@ class ListaCode extends Component {
 
 
   getQueue() {
-
     return (
       <div>
-        {this.state.codice.map((codice, index) => (
+        {this.state.idQueue.map((idQueue, index) => (
           <div  >
             <br />
-            <Card className="QCard" >
+            <Card className="QCard" style={{ width: '18rem' }} >
               {/* <Card.Img variant="top" src={this.state.image[index]} /> */}
               <Card.Body>
-                <Card.Title>{this.state.titletxt[index]}  </Card.Title>
+                <Card.Title>{this.state.title[index]}  </Card.Title>
                 <Card.Text>
                   {this.state.description[index]}
                   <br />
-                  {this.state.number[index]}
+                  {this.state.numWait[index]}
                 </Card.Text>
-                <Button className ='btnAdd'>< TiPlus /></Button>
-                
+                <Button className ='btnAdd' size="lg" block>< TiPlus /></Button>                
               </Card.Body>
             </Card>
             <br />
@@ -182,11 +183,7 @@ class ListaCode extends Component {
       </div>
     )
   }
-
-
-
-
-  componentDidMount() {
+  componentWillMount() {
     this.showQueue()
   }
 
@@ -199,15 +196,9 @@ class ListaCode extends Component {
         {/*    IF AZIENDA    */}
 
         {/* {this.getQueueForm()} */}
-
-
-
-
         {/*    IF UTENTE    */}
         <p>Code Disponibili:</p>
         {this.getQueue()}
-
-
       </div>
     );
   }
